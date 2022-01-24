333a725d9e36ccbe031b3577e5bac340fefe4d7b
Richard Kendy Tanaka <kendy_t88@hotmail.com>
2022-01-24 15:35:38 -0300

    Completed project

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,31 @@
 //import logo from './logo.svg';
 // import './App.css';
 
-import { CssBaseline, Grid, makeStyles } from "@material-ui/core";
+import { CssBaseline, Grid, makeStyles, ThemeProvider } from "@material-ui/core";
+import { theme } from "./theme";
 
 import NavBar from './components/NavBar'
 import LeftBar from './components/LeftBar';
-// import Main from './components/main/Main';
-// import RightBar from './components/rightBar/RightBar';
 import Employees from './pages/Employees/Employees';
 
-const useStyles = makeStyles((theme) => ({
-  right: {
-    [theme.breakpoints.down("sm")]: {
-      display: "none",
-    },
+const useStyles = makeStyles({
+  appMain: {
+    paddingLeft: '300px',
+    width: '100%'
   }
-}));
+})
 
 const App = () => {
   const classes = useStyles();
   return (
-    <div>
-      <NavBar />
-      <Grid container>
-        <Grid item sm={2} xs={2}>
-          <LeftBar />
-        </Grid>
-        <Grid item sm={10} xs={10}>  
-          {/* <Main /> */}
-          <Employees />
-        </Grid>
-        {/* <Grid item sm={3} className={classes.right}>
-          <RightBar />
-        </Grid> */}
-      </Grid>
-      <CssBaseline/>
-    </div>
+    <ThemeProvider theme={theme}>
+        <LeftBar />
+      <div className={classes.appMain}>
+        <NavBar />
+        <Employees />
+      </div>
+      <CssBaseline />
+    </ThemeProvider>
   );
 }
 
